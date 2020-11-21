6f8ca34882c35c13c1a80e0414e04fee4147af7d
Jean Choi <choijean@pdx.edu>
2020-11-20 17:12:06 -0800

    add script to convert all 4 csv files

--- convert_pandas2.py
+++ convert_pandas2.py
@@ -4,7 +4,7 @@ import json
 stationsdf = pd.read_csv("freeway_stations.csv")
 detectorsdf = pd.read_csv("freeway_detectors.csv")
 highwaysdf = pd.read_csv("highways.csv")
-loopdf = pd.read_csv("smallloop.csv")
+loopdf = pd.read_csv("freeway_loopdata_onehour.csv")
 
 # keeping only relevant columns for detectors
 cols = ['detectorid', 'detectorclass', 'lanenumber', 'stationid']
@@ -13,16 +13,24 @@ new_detectorsdf = detectorsdf[cols]
 # station and highway dataframe
 stationsAndHighways = stationsdf.merge(highwaysdf, on="highwayid", how="left")
 
+# merge data into detectors
+d = new_detectorsdf.merge(
+        loopdf
+        .groupby("detectorid")
+        .apply(lambda g: g.to_dict(orient="records"))
+        .reset_index(name="loopdata"),
+        how="left", on="detectorid")
+# print(d)
+
 # merge detectors into stations
 data = stationsAndHighways.merge(
-        new_detectorsdf
+        d
         .groupby("stationid")
         .apply(lambda g: g.to_dict(orient="records"))
         .reset_index(name="detectors"),
-        how="left", on="stationid")
-    .to_json(orient="records")
+        how="left", on="stationid").to_json(orient="records")
 # print(data)
 
 # write to json
-with open("test_pandas.json", "w") as jsonFile:
-    jsonFile.write(json.dumps(data, indent=4))
+with open("test_pandas2.json", "w") as jsonFile:
+    jsonFile.write(json.dumps(json.loads(data), indent=4))
