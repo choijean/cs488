50b710dbd766e9e84cc66c817517335696357cd9
Jean Choi <choijean@pdx.edu>
2020-11-23 10:00:50 -0800

    update convert script

--- convert_pandas2.py
+++ convert_pandas2.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import json
 
+# read csv documents
 stationsdf = pd.read_csv("freeway_stations.csv")
 detectorsdf = pd.read_csv("freeway_detectors.csv")
 highwaysdf = pd.read_csv("highways.csv")
-loopdf = pd.read_csv("freeway_loopdata_onehour.csv")
+loopdf = pd.read_csv("freeway_loopdata.csv")
 
 # keeping only relevant columns for detectors
 cols = ['detectorid', 'detectorclass', 'lanenumber', 'stationid']
@@ -15,22 +16,22 @@ stationsAndHighways = stationsdf.merge(highwaysdf, on="highwayid", how="left")
 
 # merge data into detectors
 d = new_detectorsdf.merge(
-        loopdf
-        .groupby("detectorid")
-        .apply(lambda g: g.to_dict(orient="records"))
-        .reset_index(name="loopdata"),
-        how="left", on="detectorid")
+    loopdf
+    .groupby("detectorid")
+    .apply(lambda g: g.to_dict(orient="records"))
+    .reset_index(name="loopdata"),
+    how="left", on="detectorid")
 # print(d)
 
 # merge detectors into stations
 data = stationsAndHighways.merge(
-        d
-        .groupby("stationid")
-        .apply(lambda g: g.to_dict(orient="records"))
-        .reset_index(name="detectors"),
-        how="left", on="stationid").to_json(orient="records")
+    d
+    .groupby("stationid")
+    .apply(lambda g: g.to_dict(orient="records"))
+    .reset_index(name="detectors"),
+    how="left", on="stationid").to_json(orient="records")
 # print(data)
 
 # write to json
-with open("test_pandas2.json", "w") as jsonFile:
+with open("test_pandas_all.json", "w") as jsonFile:
     jsonFile.write(json.dumps(json.loads(data), indent=4))
