4f2ec63c5996c740b9642f2c221ca2b2d614bf7a
Jean Choi <choijean@pdx.edu>
2020-11-28 19:52:35 -0800

    add other clean data

--- clean.py
+++ clean.py
@@ -10,7 +10,7 @@ import json
 stationsdf = pd.read_csv("freeway_stations.csv")
 detectorsdf = pd.read_csv("freeway_detectors.csv")
 highwaysdf = pd.read_csv("highways.csv")
-loopdf = pd.read_csv("freeway_loopdata.csv", parse_dates=['starttime'])
+loopdf = pd.read_csv("freeway_loopdata_oneday.csv", parse_dates=['starttime'])
 
 # clean loop data by removing null/0 speeds
 cleanloopdf = loopdf[loopdf['speed'].notna()]
@@ -46,5 +46,5 @@ data = (
 )
 
 # write to json
-data.to_json(r'clean_all.json', orient='records',
+data.to_json(r'clean_oneday.json', orient='records',
              indent=4, date_format="iso")
